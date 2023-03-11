d4676e5ebd8935a2da14dd41e70371e6d37bdcb0
Kata <yatutkata@gmail.com>
2023-03-11 16:57:55 +0300

    Add Example 005 Condition if else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Fill in Username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+Console.WriteLine("Wow, It's MASHA!");
+}
+else 
+{
+Console.Write("Hello ");
+Console.WriteLine(username);
+}
